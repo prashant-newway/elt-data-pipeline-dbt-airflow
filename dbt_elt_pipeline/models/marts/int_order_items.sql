10e10428ad3bf3dce86f216f0a602be5d44c8c77
prashant <107356705+prashant-newway@users.noreply.github.com>
2024-09-15 17:26:32 +0530

    created aggregate intermediate table , fact table .Compilation Error

--- dbt_elt_pipeline/models/marts/int_order_items.sql
+++ dbt_elt_pipeline/models/marts/int_order_items.sql
@@ -1,5 +1,3 @@
---intermediate table
-
 select
     line_item.order_item_key,
     line_item.part_key,
@@ -8,12 +6,11 @@ select
     orders.order_key,
     orders.customer_key,
     orders.order_date,
-    {{discounted_amount('line_item.extended_price' , 'line_item.discount_percentage')}} as item_discount_amount
-from 
-    {{ref('stg_tpch_orders')}} as orders
-join 
-    {{ref('stg_tpch_line_items')}} as line_item
-    on orders.order_key = line_item.order_key
-
-order by 
+    {{ discounted_amount('line_item.extended_price', 'line_item.discount_percentage') }} as item_discount_amount
+from
+    {{ ref('stg_tpch_orders') }} as orders
+join
+    {{ ref('stg_tpch_line_items') }} as line_item
+        on orders.order_key = line_item.order_key
+order by
     orders.order_date
