0407866272f6b9eca7a2e8de964ead55e115fea9
prashant <107356705+prashant-newway@users.noreply.github.com>
2024-09-22 15:35:51 +0530

    some changes

--- dbt-dag-af/dags/dbt_dag.py
+++ dbt-dag-af/dags/dbt_dag.py
@@ -10,17 +10,21 @@ profile_config = ProfileConfig(
     target_name="dev",
     profile_mapping=SnowflakeUserPasswordProfileMapping(
         conn_id="snowflake_conn", 
-        profile_args={"database": "DB_DBT_ELT", "schema": "SCHEMA_DBT_ELT"},
+        profile_args={"database": "db_dbt_elt", "schema": "schema_dbt_elt"},
     )
 )
+DBT_PROJECT_DIR = "/home/prashant-newway/Documents/DataEngineering/de2-dbt-elt/dbt-dag-af/include/dbt_elt_pipeline"
+#DBT_PROFILES_DIR = "home/prashant-newway/Documents/DataEngineering/de2-dbt-elt/dbt-dag-af/include/"
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt_elt_sf/dbt_elt_pipeline",),
+    project_config=ProjectConfig(DBT_PROJECT_DIR,),
+    #profile_config=ProfileConfig(profiles_dir=DBT_PROFILES_DIR),
     operator_args={"install_deps": True},
     profile_config=profile_config,
-    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/.venv-dbt/bin/dbt",),
+    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/.venv-dbt-af/bin/dbt",),
     schedule_interval="@daily",
-    start_date=datetime(2024, 9, 19),
+    start_date=datetime(2024, 9, 20),
     catchup=False,
-    dag_id="dbt_elt_sf_dag",
+    dag_id="dbt_elt_sf__af_dag",
 )
+
